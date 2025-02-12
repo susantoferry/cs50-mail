30c0da79a3d1e2e9f85c81bb8af20092a9c71fe6
susantoferry <yohanesfersusanto@gmail.com>
2025-02-12 23:56:45 +1100

    update database setting into tmp

--- project3/settings.py
+++ project3/settings.py
@@ -77,7 +77,8 @@ WSGI_APPLICATION = 'project3.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),
     }
 }
 
